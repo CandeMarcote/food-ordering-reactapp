262fe8a9281504c7efdf2c97ca92accbb9a4ada7
CandeMarcote <candemarcote@gmail.com>
2022-05-01 01:31:46 -0300

    first commit up to class 142

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Layout/Header";
+import Meals from "./Meal/Meals";
+import Cart from "./Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
+      <Meals />
+    </>
   );
 }
 
---
+++ src/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from './Header.module.css';
+import reactMeals from '../Assets/meals.jpg';
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+    <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton onShow={props.onShowCart} />
+    </header>
+    <div className={classes['main-image']}>
+        <img src={reactMeals} alt="a table full of delicious food!" />
+    </div>
+    </>
+  )
+}
+
+export default Header
